acb00fa38802158afc5ab0557925d36f629baa10
reaz <sebastian.langner00@gmail.com>
2021-12-05 14:23:12 +0100

    fix example

--- README.md
+++ README.md
@@ -4,11 +4,12 @@ Modification of actual ghost-cursor for puppetter, with more functionality.
 ```
 git clone https://github.com/Xetera/ghost-cursor.git cursor
 cd cursor
+npm install
 ```
 
-example of usage in src/test.ts
-to run test use
+example of usage in src/example.ts
+to run example use
 ```
-npm run test
+npm run example
 ```
 if you use wsl2 don't forget to set up displayer
---
+++ src/example.ts
@@ -0,0 +1,34 @@
+import { chromium } from 'playwright';
+import { createCursor } from './cursor';
+
+(async () => {
+	try {
+		const browser = await chromium.launch({
+			channel: 'chrome',
+			headless: false,
+		});
+		const browserContext = await browser.newContext({
+			viewport: null,
+		});
+
+		const page = await browserContext.newPage();
+		const cursor = await createCursor(page);
+		await page.goto('https://www.google.com');
+
+		await page.waitForTimeout(5000);
+		await cursor.actions.moveTo({ x: 50, y: 10 });
+		await cursor.actions.moveTo({ x: 700, y: 700 });
+		await cursor.actions.moveTo({ x: 50, y: 10 });
+		await cursor.actions.move('#L2AGLb > div');
+		await cursor.actions.moveTo({ x: 50, y: 10 });
+		await cursor.actions.move('#L2AGLb > div');
+		await cursor.actions.click({ delayMin: 20, delayMax: 50 });
+		await cursor.actions.move(
+			'body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input'
+		);
+		await cursor.actions.click({ delayMin: 20, delayMax: 50 }, true);
+	} catch (error: any) {
+		console.log('here');
+		console.log(error.message);
+	}
+})();
