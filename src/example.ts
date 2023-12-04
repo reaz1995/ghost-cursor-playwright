08f60279c70bc1ba21554c2d409d80e91c65bb1b
git-avilabs <mail.avinashsah@gmail.com>
2023-12-04 16:07:47 +0530

    TrustedHTML fix

--- src/example.ts
+++ src/example.ts
@@ -1,60 +1,71 @@
-import { chromium } from 'playwright-core';
-import { createCursor } from './cursor';
-import { Vector } from './math';
+import { chromium } from "playwright-core";
+import { createCursor } from "./cursor";
+import { Vector } from "./math";
 
 (async () => {
-	try {
-		const browser = await chromium.launch({
-			channel: 'chrome',
-			headless: false,
-		});
-		const browserContext = await browser.newContext({
-			viewport: null,
-		});
+  try {
+    const browser = await chromium.launch({
+      channel: "chrome",
+      headless: false,
+    });
+    const browserContext = await browser.newContext({
+      viewport: null,
+    });
 
-		const page = await browserContext.newPage();
-		const cursor = await createCursor(page);
-		await page.goto('https://www.google.com');
+    const page = await browserContext.newPage();
+    const cursor = await createCursor(page);
+    await page.goto("https://www.google.com");
 
-		const randomPoint = await cursor.getRandomPointOnViewport();
+    const randomPoint = await cursor.getRandomPointOnViewport();
 
-		await page.waitForTimeout(5000);
-		await cursor.actions.move(randomPoint);
+    await page.waitForTimeout(5000);
 
-		await cursor.actions.move({ x: 70, y: 70 });
-		await cursor.actions.move({ x: 500, y: 500 }, { waitBeforeMove: [500, 1_500] });
-		await cursor.actions.move({ x: 50, y: 10 });
-		await cursor.actions.move('#L2AGLb > div', { paddingPercentage: 30 });
-		await cursor.actions.move(randomPoint);
-		await cursor.actions.move('#L2AGLb > div', {
-			paddingPercentage: 50,
-			waitBeforeMove: [1_000, 2_000],
-			waitForSelector: 30_000,
-		});
+    await cursor.actions.move(randomPoint);
 
-		await cursor.actions.click(
-			{ target: '#L2AGLb > div', waitBetweenClick: [20, 50] },
-			{
-				paddingPercentage: 50,
-				waitBeforeMove: [1_000, 2_000],
-				waitForSelector: 30_000,
-			}
-		);
-		await cursor.actions.click({
-			target:
-				'body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input',
-		});
-		await cursor.actions.click({ waitBeforeClick: [500, 1_000], waitBetweenClick: [20, 50] });
-		await cursor.actions.move({ x: 50, y: 10 });
-		await cursor.actions.click(
-			{
-				target:
-					'body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input',
-			},
-			{ paddingPercentage: 70, waitBeforeMove: [500, 2_500] }
-		);
-		await cursor.actions.click({ waitBetweenClick: [20, 50], doubleClick: true });
-	} catch (error: any) {
-		console.log(error.message);
-	}
+    await cursor.actions.move({ x: 70, y: 70 });
+
+    await cursor.actions.move(
+      { x: 500, y: 500 },
+      { waitBeforeMove: [500, 1_500] }
+    );
+    await cursor.actions.move({ x: 50, y: 10 });
+    await cursor.actions.move("#L2AGLb > div", { paddingPercentage: 30 });
+    await cursor.actions.move(randomPoint);
+    await cursor.actions.move("#L2AGLb > div", {
+      paddingPercentage: 50,
+      waitBeforeMove: [1_000, 2_000],
+      waitForSelector: 30_000,
+    });
+
+    await cursor.actions.click(
+      { target: "#L2AGLb > div", waitBetweenClick: [20, 50] },
+      {
+        paddingPercentage: 50,
+        waitBeforeMove: [1_000, 2_000],
+        waitForSelector: 30_000,
+      }
+    );
+    await cursor.actions.click({
+      target:
+        "body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input",
+    });
+    await cursor.actions.click({
+      waitBeforeClick: [500, 1_000],
+      waitBetweenClick: [20, 50],
+    });
+    await cursor.actions.move({ x: 50, y: 10 });
+    await cursor.actions.click(
+      {
+        target:
+          "body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input",
+      },
+      { paddingPercentage: 70, waitBeforeMove: [500, 2_500] }
+    );
+    await cursor.actions.click({
+      waitBetweenClick: [20, 50],
+      doubleClick: true,
+    });
+  } catch (error: any) {
+    console.log(error.message);
+  }
 })();
--- src/mouse-helper.ts
+++ src/mouse-helper.ts
@@ -1,13 +1,20 @@
-import playwright from 'playwright-core';
+import playwright from "playwright-core";
 // This injects a box into the page that moves with the mouse;
 // Useful for debugging
 // Will be reattached every page load
 function installMouseHelper(page: playwright.Page): void {
-	page.on('load', () => {
-		page.evaluate(() => {
-			const box = document.createElement('p-mouse-pointer');
-			const styleElement = document.createElement('style');
-			styleElement.innerHTML = `
+  page.on("load", () => {
+    page
+      .evaluate(() => {
+        // @ts-ignore
+        window.escapeHTMLPolicy = trustedTypes.createPolicy("forceInner", {
+          createHTML: (to_escape) => to_escape,
+        });
+        const box = document.createElement("p-mouse-pointer");
+        const styleElement = document.createElement("style");
+        // @ts-ignore
+        styleElement.innerHTML = window.escapeHTMLPolicy.createHTML(
+          `
           p-mouse-pointer {
             pointer-events: none;
             position: absolute;
@@ -46,62 +53,64 @@ function installMouseHelper(page: playwright.Page): void {
           p-mouse-pointer-hide {
             display: none
           }
-        `;
-			document.head.appendChild(styleElement);
-			document.body.appendChild(box);
-			document.addEventListener(
-				'mousemove',
-				(event) => {
-					box.style.left = String(event.pageX) + 'px';
-					box.style.top = String(event.pageY) + 'px';
-					box.classList.remove('p-mouse-pointer-hide');
-					updateButtons(event.buttons);
-				},
-				true
-			);
-			document.addEventListener(
-				'mousedown',
-        ( event ) => {
-					updateButtons(event.buttons);
-					box.classList.add('button-' + String(event.which));
-					box.classList.remove('p-mouse-pointer-hide');
-				},
-				true
-			);
-			document.addEventListener(
-				'mouseup',
-				(event) => {
-					updateButtons(event.buttons);
-					box.classList.remove('button-' + String(event.which));
-					box.classList.remove('p-mouse-pointer-hide');
-				},
-				true
-			);
-			document.addEventListener(
-				'mouseleave',
-				(event) => {
-					updateButtons(event.buttons);
-					box.classList.add('p-mouse-pointer-hide');
-				},
-				true
-			);
-			document.addEventListener(
-				'mouseenter',
-				(event) => {
-					updateButtons(event.buttons);
-					box.classList.remove('p-mouse-pointer-hide');
-				},
-				true
-			);
-			/* eslint-disable */
-			function updateButtons(buttons) {
-				for (let i = 0; i < 5; i++) {
-					// @ts-ignore
-					box.classList.toggle('button-' + String(i), buttons & (1 << i));
-				}
-			}
-		}).catch(() => {});
-	});
+        `
+        );
+        document.head.appendChild(styleElement);
+        document.body.appendChild(box);
+        document.addEventListener(
+          "mousemove",
+          (event) => {
+            box.style.left = String(event.pageX) + "px";
+            box.style.top = String(event.pageY) + "px";
+            box.classList.remove("p-mouse-pointer-hide");
+            updateButtons(event.buttons);
+          },
+          true
+        );
+        document.addEventListener(
+          "mousedown",
+          (event) => {
+            updateButtons(event.buttons);
+            box.classList.add("button-" + String(event.which));
+            box.classList.remove("p-mouse-pointer-hide");
+          },
+          true
+        );
+        document.addEventListener(
+          "mouseup",
+          (event) => {
+            updateButtons(event.buttons);
+            box.classList.remove("button-" + String(event.which));
+            box.classList.remove("p-mouse-pointer-hide");
+          },
+          true
+        );
+        document.addEventListener(
+          "mouseleave",
+          (event) => {
+            updateButtons(event.buttons);
+            box.classList.add("p-mouse-pointer-hide");
+          },
+          true
+        );
+        document.addEventListener(
+          "mouseenter",
+          (event) => {
+            updateButtons(event.buttons);
+            box.classList.remove("p-mouse-pointer-hide");
+          },
+          true
+        );
+        /* eslint-disable */
+        function updateButtons(buttons) {
+          for (let i = 0; i < 5; i++) {
+            // @ts-ignore
+            box.classList.toggle("button-" + String(i), buttons & (1 << i));
+          }
+        }
+      })
+      .catch(() => {});
+  });
 }
 
 export default installMouseHelper;
